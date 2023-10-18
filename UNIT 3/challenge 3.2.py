d6f44e5ebd852770744a80977295a551bafaa4be
asmsu10420221041401206 <144765166+asmsu10420221041401206@users.noreply.github.com>
2023-10-18 17:33:12 +0530

    Add files via upload

---
+++ UNIT 3/challenge 3.2.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+students = [
+    Student("John", "A001", 3.8),
+    Student("Alice", "A002", 3.5),
+    Student("Bob", "A003", 3.9),
+]
+
+sort_students(students)
+
+for student in students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
